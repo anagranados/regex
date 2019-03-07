606d99440084efbdf5d55680319c2a36adefd7ba
anagranados <43890650+anagranados@users.noreply.github.com>
2019-03-07 23:14:49 +0100

    k

--- validation.js
+++ validation.js
@@ -1,7 +1,14 @@
 //validation scrpt here
-
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
 const patterns={
-    telephone:/\d{11}/
-}
+    telephone:/^\d{11}$/
+};
 
-//validation function
+//validation function 
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', (e)=>{
+    console.log(e.target.attributes.name.value);
+    });
+});
