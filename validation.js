e7eac5b675597a9727fdb641a3a55a6520c47fb2
anagranados <43890650+anagranados@users.noreply.github.com>
2019-03-08 16:37:10 +0100

    r

--- validation.js
+++ validation.js
@@ -2,13 +2,33 @@
 const inputs = document.querySelectorAll('input');
 console.log(inputs);
 const patterns={
-    telephone:/^\d{11}$/
+    telephone:/^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
+
+
+
 //validation function 
+function validate (field, regex){
+ if(regex.test(field.value)){
+     field.className ='valid';
+     console.log(field.className);
+ }
+ else{
+     field.className ='invalid';
+     //console.log(field.className);
+ }
+    //console.log(regex.test(field.value));
+}
+
 
 inputs.forEach((input)=>{
     input.addEventListener('keyup', (e)=>{
     console.log(e.target.attributes.name.value);
+    validate(e.target,patterns[e.target.attributes.name.value]);
     });
 });
